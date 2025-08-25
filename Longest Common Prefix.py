db0bbccccba237875c5185999220131941514630
Marcel2 <marcel2@Marcels-MacBook-Pro.local>
2025-08-25 17:43:45 -0400

    second commit

--- Longest Common Prefix.py
+++ Longest Common Prefix.py
@@ -22,9 +22,32 @@
 # strs[i] consists of only lowercase English letters if it is non-empty.
 
 class Solution(object):
-    def longestCommonPrefix(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: str
-        """
+    def longestCommonPrefix(self, strs):        
+        strs_length = len(strs[0])
+        # Store the length of the first string as our maximum possible prefix length
         
+        if len(strs) == 1:
+        # Check if the array contains only one string
+            return strs[0]
+            # If only one string exists, return it as the longest common prefix
+        
+        for i in range(strs_length):
+        # Loop from 0 to (length of first string - 1)
+        # This will try prefixes of decreasing length: full string, then one char less, etc.
+        
+            for_check = strs[0][:(strs_length-i)]
+            # Create a candidate prefix by taking characters from start of first string
+            # Length = (original_length - current_iteration)
+            # i=0: take full string, i=1: take all but last char, i=2: take all but last 2 chars, etc.
+            
+            is_good = all(s.startswith(for_check) for s in strs)
+            # Check if ALL strings in the array start with the current candidate prefix
+            # all() returns True only if every string starts with for_check
+            
+            if is_good:
+            # If the current candidate prefix is found in all strings
+                return for_check
+                # PSEUDOCODE: Return this prefix (it's the longest one that works)
+        
+        return ""
+        # If no common prefix found after trying all possibilities, return empty string
